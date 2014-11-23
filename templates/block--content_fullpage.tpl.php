c2b155e56ee416c4ab2df00ef673ce468417f0a0
Schmook <eschmidt@vdesign.com>
2014-11-23 17:14:18 -0600

    view template edits

--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -9,16 +9,10 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="section <?php print $title; ?> <?php print $classes; ?>"<?php print $attributes; ?> data-anchor="section<?php print $block_id; ?>" >
 
-  <?php if ($title=="hi-neighbor"): ?>
-  	<img src="/sites/default/files/images/down-arrow.png" class="down-arrow" alt="down-arrow" />
-  <?php endif; ?>
-
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
   <?php	$title = str_replace("-", " ", $title); ?>
-  	<div class="title-stripe"></div>
-    <h2<?php print $title_attributes; ?>><span>&bull;</span><?php print $title; ?><span>&bull;</span></h2>
-  	<div class="title-stripe below"></div>
+    <h2<?php print $title_attributes; ?><?php print $title; ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
   <?php print $content; ?>
