4fb0ebe43b73f1c175f036be7de1d5c8c2a9933c
Schmook <eschmidt@vdesign.com>
2014-11-23 18:02:05 -0600

    fullpage options

--- js/script.js
+++ js/script.js
@@ -34,7 +34,6 @@ Drupal.behaviors.my_custom_behavior = {
         $('#fullpage').fullpage({
             scrollingSpeed: 700,
             autoScrolling: false,
-            anchors:['about', 'prize-package', 'startup', 'our-partners', 'register-to-attend'],
             navigationTooltips: ['about', 'prize-package', 'startup', 'our-partners', 'register-to-attend']
         });
     }
