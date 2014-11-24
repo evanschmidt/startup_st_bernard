e4100f70687f0ec988ef67187b1e11304b0f2fc1
Schmook <eschmidt@vdesign.com>
2014-11-23 20:16:26 -0600

    prize package conditional

--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -11,7 +11,7 @@
 
   <?php if ($title=="prize-package"): ?>
   	Hello
-  <?php else; ?>
+  <?php endif; ?>
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
@@ -21,6 +21,5 @@
   <?php print render($title_suffix); ?>
   <?php print $content; ?>
 
-  <?php endif; ?>
 
 </div>
