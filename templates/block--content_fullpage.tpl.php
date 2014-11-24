c3e2f6d6c6c5cfc0d4e1375f8a4243a58f7df3ef
Schmook <eschmidt@vdesign.com>
2014-11-23 20:23:16 -0600

    prize package conditional

--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -10,6 +10,7 @@
 <div id="<?php print $block_html_id; ?>" class="section <?php print $title; ?> <?php print $classes; ?>"<?php print $attributes; ?> data-anchor="section<?php print $block_id; ?>" >
 
   <?php if ($title=="prize-package"): ?>
+  	hello
 
 	  <?php print render($title_prefix); ?>
 	  <?php if ($title): ?>
@@ -17,6 +18,16 @@
 	    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 	  <?php endif; ?>
 	  <?php print render($title_suffix); ?>
+
+  <?php else : ?>
+
+	  <?php print render($title_prefix); ?>
+	  <?php if ($title): ?>
+	  <?php	$title = str_replace("-", " ", $title); ?>
+	    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+	  <?php endif; ?>
+	  <?php print render($title_suffix); ?>
+
   <?php endif; ?>
 
   <?php print $content; ?>
