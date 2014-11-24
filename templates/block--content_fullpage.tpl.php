66ecd9936f16850552be99d0343923cdac3afc27
Schmook <eschmidt@vdesign.com>
2014-11-23 20:26:32 -0600

    prize package conditional

--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -9,25 +9,18 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="section <?php print $title; ?> <?php print $classes; ?>"<?php print $attributes; ?> data-anchor="section<?php print $block_id; ?>" >
 
-  <?php if ($title=="prize-package"): ?>
 
 	  <?php print render($title_prefix); ?>
 	  <?php if ($title): ?>
 	  <?php	$title = str_replace("-", " ", $title); ?>
-	    <h2<?php print $title_attributes; ?>>$110,000 Prize Package</h2>
+		  <?php if ($title=="prize-package"): ?>
+			    <h2<?php print $title_attributes; ?>>$110,000 Prize Package</h2>
+		  <?php else : ?>
+			    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+		  <?php endif; ?>
 	  <?php endif; ?>
 	  <?php print render($title_suffix); ?>
 
-  <?php else : ?>
-
-	  <?php print render($title_prefix); ?>
-	  <?php if ($title): ?>
-	  <?php	$title = str_replace("-", " ", $title); ?>
-	    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
-	  <?php endif; ?>
-	  <?php print render($title_suffix); ?>
-
-  <?php endif; ?>
 
   <?php print $content; ?>
 
