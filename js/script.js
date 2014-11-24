fb6793322ede62524261284ce2716d17af349b32
Schmook <eschmidt@vdesign.com>
2014-11-23 21:36:03 -0600

    fixed .front

--- js/script.js
+++ js/script.js
@@ -42,8 +42,7 @@ Drupal.behaviors.my_custom_behavior = {
             responsize: 1
         });
     }
- 
-
+    
   }
 };
 
--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -9,7 +9,6 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="section <?php print $title; ?> <?php print $classes; ?>"<?php print $attributes; ?> data-anchor="section<?php print $block_id; ?>" >
 
-
 	  <?php print render($title_prefix); ?>
 	  <?php if ($title): ?>
 	  <?php	$title = str_replace("-", " ", $title); ?>
@@ -20,7 +19,6 @@
 		  <?php endif; ?>
 	  <?php endif; ?>
 	  <?php print render($title_suffix); ?>
-
 
   <?php print $content; ?>
 
