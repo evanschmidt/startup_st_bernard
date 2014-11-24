926718ee428856ea66466d2a81b933de735df781
Schmook <eschmidt@vdesign.com>
2014-11-23 21:20:35 -0600

    enter the challenge layout

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ Drupal.behaviors.my_custom_behavior = {
     if(jQuery('body.front').length > 0){
        $("#block-block-3").backstretch("/sites/default/files/images/StartUpStBernard-logoBG.jpg");
        $(".how-it-works .block-title").backstretch("/sites/default/files/images/StartUpStBernard-worksBG.jpg");
-//        $("#block-views-our-farmers-block-1").backstretch("/sites/default/files/images/page/field.jpg");
+       $(".enter-the-challenge").backstretch("/sites/default/files/images/StartUpStBernard-registerBG.jpg");
 //        $("#block-block-11").backstretch([
 //            "/sites/default/files/images/homepage/slider1.jpg"
 //          , "/sites/default/files/images/homepage/slider2.jpg"
