112e4b67be4c216715edaa134d293a39916f65b5
Schmook <eschmidt@vdesign.com>
2014-11-23 18:31:07 -0600

    fullpage options

--- js/script.js
+++ js/script.js
@@ -34,8 +34,8 @@ Drupal.behaviors.my_custom_behavior = {
         $('#fullpage').fullpage({
             scrollingSpeed: 700,
             autoScrolling: false,
-            anchors:['about', 'prize-package', 'startup', 'our-partners', 'register-to-attend'],
-            navigationTooltips: ['about', 'prize-package', 'startup', 'our-partners', 'register-to-attend'],
+            anchors:['about', 'prize-package', 'senter-the-challange', 'our-partners', 'register-to-attend'],
+            navigationTooltips: ['about', 'prize-package', 'enter-the-challange', 'our-partners', 'register-to-attend'],
             verticalCentered: true,
             paddingTop: '0',
             paddingBottom: '0',
