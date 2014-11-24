c75aeaae4ef6287c0dd8e9dca5006f6151e7071b
Schmook <eschmidt@vdesign.com>
2014-11-23 22:14:59 -0600

    fullpage options

--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ Drupal.behaviors.my_custom_behavior = {
             navigationTooltips: ['about', 'prize-package', 'how-it-works', 'enter-the-challenge', 'our-partners', 'register-to-attend'],
             verticalCentered: true,
             resize : false,
+            paddingTop: '4em',
+            paddingBottom: '1em',
             css3: false,
             responsize: 1
         });
